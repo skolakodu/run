2aa436bb1304f757aeff6e340f86670f57384a1f
Roman Amelin <skolakodu@gmail.com>
2023-06-09 09:05:36 +0600

    Sekce proč my a nabídka koloběžek

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$('.scooters-slider').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    prevArrow: ".scooters-arrows__prev",
+    nextArrow: ".scooters-arrows__next",
+  });
