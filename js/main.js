c75af8c632cee482027c096872106e2ec09606b4
Roman Amelin <skolakodu@gmail.com>
2023-06-09 09:34:13 +0600

    Sekce s recenzemi

--- js/main.js
+++ js/main.js
@@ -4,4 +4,12 @@ $('.scooters-slider').slick({
     slidesToScroll: 1,
     prevArrow: ".scooters-arrows__prev",
     nextArrow: ".scooters-arrows__next",
-  });
+});
+
+$('.reviews-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: ".reviews-arrows__prev",
+    nextArrow: ".reviews-arrows__next",
+});
